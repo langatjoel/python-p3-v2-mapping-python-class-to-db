3bce2e968a7877ee4d8ceb7d99fe5a8728b88013
joel langat <langatjoel42@gmail.com>
2024-06-04 22:14:38 +0300

    made some solutions

--- lib/debug.py
+++ lib/debug.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
-#lib/testing/debug.py
 
 from __init__ import CONN, CURSOR
 from department import Department
 
 import ipdb
+
+Department.drop_table()
+Department.create_table()
 
 ipdb.set_trace()
